623e9df8f007c055e27dc051f5b9c78880c8afe4
Chandramani12 <75423990+Chandramani12@users.noreply.github.com>
2021-04-19 17:00:36 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+use Main;
+create table tblperson(id int,person_name varchar(120),email varchar(130));
+insert into tblperson(id,person_name,email) values(1,'jack','kack@gmail.com');
+
+select * from tblperson;
+
+insert into tblperson(id,person_name,email) values(2,'miko','miko@gmail.com');
